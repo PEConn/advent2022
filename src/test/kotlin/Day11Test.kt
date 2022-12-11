b29f9682d7010a6463e99c15a492d2394fffa5e6
PEConn <conn.peter@gmail.com>
2022-12-11 09:02:07 +0000

    Day 11 part 2.

--- src/test/kotlin/Day11Test.kt
+++ src/test/kotlin/Day11Test.kt
@@ -7,10 +7,9 @@ typealias WorryLevel = Long
 typealias InspectionOp = (x: Long) -> Long
 typealias TargetingOp = (x: Long) -> Int
 
-class Monkey(items: List<WorryLevel>,
+data class Monkey(val items: MutableList<WorryLevel>,
              val operation: InspectionOp,
              val findTarget: TargetingOp) {
-    val items = items.toMutableList()
     var inspections = 0
 }
 
@@ -43,8 +42,8 @@ fun part1(input: List<Monkey>): Int {
     return byInspections[0].inspections * byInspections[1].inspections
 }
 
-fun part2(input: List<Monkey>): Long {
-    (0 until 10000).forEach { _ -> round(input) { it } }
+fun part2(input: List<Monkey>, divisor: Long): Long {
+    (0 until 10000).forEach { _ -> round(input) { it % divisor } }
 
     val byInspections = input.sortedByDescending { it.inspections }
     return byInspections[0].inspections.toLong() * byInspections[1].inspections.toLong()
@@ -53,38 +52,47 @@ fun part2(input: List<Monkey>): Long {
 class Day11Test {
     @Test
     fun example1() {
-        assertEquals(10605, part1(GIVEN_INPUT))
+        assertEquals(10605, part1(GIVEN_INPUT.duplicate()))
     }
 
     @Test
     fun challenge1() {
-        assertEquals(64032, part1(TEST_INPUT))
+        assertEquals(64032, part1(TEST_INPUT.duplicate()))
     }
 
     @Test
     fun example2() {
-        assertEquals(2713310158, part2(GIVEN_INPUT))
+        assertEquals(2713310158, part2(GIVEN_INPUT.duplicate(), 23 * 19 * 13 * 17))
     }
+
+    @Test
+    fun challenge2() {
+        assertEquals(0, part2(TEST_INPUT.duplicate(), 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19))
+    }
+}
+
+fun List<Monkey>.duplicate(): List<Monkey> {
+    return map { it.copy(items = it.items.toMutableList()) }
 }
 
 val GIVEN_INPUT = listOf(
     Monkey(
-        items = listOf(79, 98),
+        items = mutableListOf(79, 98),
         operation = { it * 19 },
         findTarget = { if (it % 23 == 0L) { 2 } else { 3 } }
     ),
     Monkey(
-        items = listOf(54, 65, 75, 74),
+        items = mutableListOf(54, 65, 75, 74),
         operation = { it + 6 },
         findTarget = { if (it % 19 == 0L) { 2 } else { 0 } }
     ),
     Monkey(
-        items = listOf(79, 60, 97),
+        items = mutableListOf(79, 60, 97),
         operation = { it * it },
         findTarget = { if (it % 13 == 0L) { 1 } else { 3 } }
     ),
     Monkey(
-        items = listOf(74),
+        items = mutableListOf(74),
         operation = { it + 3 },
         findTarget = { if (it % 17 == 0L) { 0 } else { 1 } }
     ),
@@ -92,42 +100,42 @@ val GIVEN_INPUT = listOf(
 
 val TEST_INPUT = listOf(
     Monkey(
-        items = listOf(83, 88, 96, 79, 86, 88, 70),
+        items = mutableListOf(83, 88, 96, 79, 86, 88, 70),
         operation = { it * 5 },
         findTarget = { if (it % 11 == 0L) { 2 } else { 3 } }
     ),
     Monkey(
-        items = listOf(59, 63, 98, 85, 68, 72),
+        items = mutableListOf(59, 63, 98, 85, 68, 72),
         operation = { it * 11 },
         findTarget = { if (it % 5 == 0L) { 4 } else { 0 } }
     ),
     Monkey(
-        items = listOf(90, 79, 97, 52, 90, 94, 71, 70),
+        items = mutableListOf(90, 79, 97, 52, 90, 94, 71, 70),
         operation = { it + 2 },
         findTarget = { if (it % 19 == 0L) { 5 } else { 6 } }
     ),
     Monkey(
-        items = listOf(97, 55, 62),
+        items = mutableListOf(97, 55, 62),
         operation = { it + 5 },
         findTarget = { if (it % 13 == 0L) { 2 } else { 6 } }
     ),
     Monkey(
-        items = listOf(74, 54, 94, 76),
+        items = mutableListOf(74, 54, 94, 76),
         operation = { it * it },
         findTarget = { if (it % 7 == 0L) { 0 } else { 3 } }
     ),
     Monkey(
-        items = listOf(58),
+        items = mutableListOf(58),
         operation = { it + 4 },
         findTarget = { if (it % 17 == 0L) { 7 } else { 1 } }
     ),
     Monkey(
-        items = listOf(66, 63),
+        items = mutableListOf(66, 63),
         operation = { it + 6 },
         findTarget = { if (it % 2 == 0L) { 7 } else { 5 } }
     ),
     Monkey(
-        items = listOf(56, 56, 90, 96, 68),
+        items = mutableListOf(56, 56, 90, 96, 68),
         operation = { it + 7 },
         findTarget = { if (it % 3 == 0L) { 4 } else { 1 } }
     ),
